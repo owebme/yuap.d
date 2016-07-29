f86c247007a84d0e035667ac188193cce6c1d67c
owebme <owebme@gmail.com>
2016-07-29 09:13:51 +0300

    ver 0.2

--- assets/js/init.js
+++ assets/js/init.js
@@ -17,4 +17,39 @@
         $(".popup").attr("data-open", "false");
     });
 
+    app.$dom.body.on("click", ".data__item__date", function(){
+        app.$dom.body.addClass("no-scroll");
+        $(".modal").attr("data-open", "true");
+    });
+
+    app.$dom.body.on("click", ".modal__button__close, .modal__footer", function(){
+        app.$dom.body.removeClass("no-scroll");
+        $(".modal").attr("data-open", "false");
+    });
+
+    app.$dom.body.on("click", ".sidebarSwither", function(){
+        var $swither = $(this),
+            $panel = $swither.closest(".data__menu__left"),
+            $wrapper = $(".content__wrapper"),
+            $dataList = $(".data__list"),
+            $dashboard = $(".dashboard");
+
+        if ($swither.hasClass("sidebarSwither--active")){
+            $swither.removeClass("sidebarSwither--active");
+            $panel.removeClass("slide-right");
+            $wrapper.removeClass("slide-right");
+            $dataList.removeClass("col-md-14 col-lg-13").addClass("col-md-13 col-lg-12");
+            $dashboard.removeClass("col-md-10 col-lg-11").addClass("col-md-11 col-lg-12");
+        }
+        else {
+            $swither.addClass("sidebarSwither--active");
+            $panel.addClass("slide-right");
+            $wrapper.addClass("slide-right");
+            $dataList.removeClass("col-md-13 col-lg-12").addClass("col-md-14 col-lg-13");
+            $dashboard.removeClass("col-md-11 col-lg-12").addClass("col-md-10 col-lg-11");
+        }
+    });
+
+
+
 })();
